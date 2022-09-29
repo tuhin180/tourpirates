4a50d6faae3c7b41749673dd401a315e9479aba2
tuhin180 <tanjirtuhin18@gmail.com>
2022-09-29 12:19:02 +0600

    data fetch completed

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Cart = (props) => {
+  const { Cost, description, img, title, duration } = props.event;
+  return <div></div>;
+};
+
+export default Cart;
--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,13 +1,24 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
+// data load
 const Event = () => {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    fetch("product.json")
+      .then((res) => res.json())
+      .then((data) => setEvents(data));
+  }, []);
   return (
     <div>
       <h1 className="text-center text-green-500 text 4xl font-semibold">
         Enjoy Our <span className="text-rose-600">Hospitality</span>
       </h1>
       <div>
-        <div className="event-container"></div>
+        <div className="event-container">
+          {events.map((event) => (
+            <Cart key={event.id} event={event}></Cart>
+          ))}
+        </div>
         <div className="cart-container"></div>
       </div>
     </div>
