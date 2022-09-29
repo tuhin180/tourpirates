82ec65d82afac0ec1acd8046d65d66b60cd91164
tuhin180 <tanjirtuhin18@gmail.com>
2022-09-29 22:39:59 +0600

    done of question site

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -3,11 +3,28 @@ import React from "react";
 const Question = () => {
   return (
     <div className="bg-yellow-200 rounded p-6">
-      <div className="bg-gray-300 rounded-sm m-2">
-        <h1>how Does react work</h1>
+      <div className="bg-gray-300  text-left rounded-sm m-2 p-4">
+        <h1 className="text-3xl">how Does react work</h1>
+        <p>
+          React is a declarative, efficient, and flexible JavaScript library for
+          building user interfaces. ‘V’ denotes the view in MVC. ReactJS is an
+          open-source, component-based front end library responsible only for
+          the view layer of the application. It is maintained by Facebook. React
+          uses a declarative paradigm that makes it easier to reason about your
+          application and aims to be both efficient and flexible. It designs
+          simple views for each state in your application, and React will
+          efficiently update and render just the right component when your data
+          changes. The declarative view makes your code more predictable and
+          easier to debug. A React application is made of multiple components,
+          each responsible for rendering a small, reusable piece of HTML.
+          Components can be nested within other components to allow complex
+          applications to be built out of simple building blocks. A component
+          may also maintain an internal state – for example, a TabList component
+          may store a variable corresponding to the currently open tab.
+        </p>
       </div>
-      <div className="bg-teal-200 rounded-sm text-left m-2">
-        <h1>Difference Between Props and state</h1>
+      <div className="bg-teal-200 rounded-sm text-left m-2 p-4">
+        <h1 className="text-3xl">Difference Between Props and state</h1>
         <h3>props</h3>
         <p>
           The Data is passed from one component to another.It is Immutable
@@ -22,7 +39,7 @@ const Question = () => {
           component. State is both read and write.
         </p>
       </div>
-      <div className="bg-lime-200 rounded-sm text-left m-2">
+      <div className="bg-lime-200 rounded-sm text-left m-2 p-4">
         <h1 className="text-3xl">UseEffect use withoud loading data</h1>
         <p>
           The main advantage of react hooks is the re-usability of stateful
